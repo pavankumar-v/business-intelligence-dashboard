b704b0b6895b09347ec0eb4dc0ffa42fcaa2f62f
Pavan Kumar V <pavankumarv124@gmail.com>
2025-12-05 22:50:28 +0530

    chore: add indicator for type=ographu

--- src/components/ui/typography.tsx
+++ src/components/ui/typography.tsx
@@ -1,4 +1,4 @@
-import { cn } from '@/lib/utils';
+import { cn } from "@/lib/utils";
 
 type TypographyProps = React.HTMLAttributes<HTMLHeadingElement> & {
   icon?: React.ReactNode;
@@ -14,8 +14,8 @@ export function TypographyH1({
   return (
     <h1
       className={cn(
-        'scroll-m-20 text-4xl font-extrabold tracking-tight text-balance',
-        className,
+        "scroll-m-20 text-4xl font-extrabold tracking-tight text-balance",
+        className
       )}
       {...props}
     >
@@ -32,8 +32,8 @@ export function TypographyH2({
   return (
     <h2
       className={cn(
-        'scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0 flex items-center gap-2',
-        className,
+        "scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0 flex items-center gap-2",
+        className
       )}
       {...props}
     >
@@ -45,16 +45,26 @@ export function TypographyH2({
 export function TypographyH3({
   children,
   className,
+  withIndicator,
+  indicatorColor,
   ...props
 }: TypographyProps) {
   return (
     <h3
       className={cn(
-        'scroll-m-20 text-2xl font-semibold tracking-tight',
-        className,
+        "scroll-m-20 text-2xl font-semibold tracking-tight flex items-center gap-2",
+        className
       )}
       {...props}
     >
+      {withIndicator && (
+        <div
+          className={cn(
+            "h-[30px] w-[14px] bg-blue-200 rounded",
+            indicatorColor
+          )}
+        ></div>
+      )}
       {children}
     </h3>
   );
@@ -70,16 +80,16 @@ export function TypographyH4({
   return (
     <h4
       className={cn(
-        'scroll-m-20 text-xl font-semibold tracking-tight flex items-center gap-2',
-        className,
+        "scroll-m-20 text-xl font-semibold tracking-tight flex items-center gap-2",
+        className
       )}
       {...props}
     >
       {withIndicator && (
         <div
           className={cn(
-            'h-[30px] w-[14px] bg-blue-200 rounded',
-            indicatorColor,
+            "h-[30px] w-[14px] bg-blue-200 rounded",
+            indicatorColor
           )}
         ></div>
       )}
@@ -95,7 +105,7 @@ export function TypographyP({
 }: React.HTMLAttributes<HTMLParagraphElement>) {
   return (
     <p
-      className={cn('leading-7 [&:not(:first-child)]:mt-6', className)}
+      className={cn("leading-7 [&:not(:first-child)]:mt-6", className)}
       {...props}
     >
       {children}
@@ -110,7 +120,7 @@ export function TypographyBlockquote({
 }: React.HTMLAttributes<HTMLQuoteElement>) {
   return (
     <blockquote
-      className={cn('mt-6 border-l-2 pl-6 italic', className)}
+      className={cn("mt-6 border-l-2 pl-6 italic", className)}
       {...props}
     >
       {children}
@@ -126,8 +136,8 @@ export function TypographyInlineCode({
   return (
     <code
       className={cn(
-        'bg-muted relative rounded px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold',
-        className,
+        "bg-muted relative rounded px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold",
+        className
       )}
       {...props}
     >
@@ -142,7 +152,7 @@ export function TypographyLead({
   ...props
 }: React.HTMLAttributes<HTMLParagraphElement>) {
   return (
-    <p className={cn('text-muted-foreground text-xl', className)} {...props}>
+    <p className={cn("text-muted-foreground text-xl", className)} {...props}>
       {children}
     </p>
   );
@@ -154,7 +164,7 @@ export function TypographyLarge({
   ...props
 }: React.HTMLAttributes<HTMLDivElement>) {
   return (
-    <div className={cn('text-lg font-semibold', className)} {...props}>
+    <div className={cn("text-lg font-semibold", className)} {...props}>
       {children}
     </div>
   );
@@ -167,7 +177,7 @@ export function TypographySmall({
 }: React.HTMLAttributes<HTMLSpanElement>) {
   return (
     <small
-      className={cn('text-sm leading-none font-medium', className)}
+      className={cn("text-sm leading-none font-medium", className)}
       {...props}
     >
       {children}
@@ -181,7 +191,7 @@ export function TypographyMuted({
   ...props
 }: React.HTMLAttributes<HTMLParagraphElement>) {
   return (
-    <p className={cn('text-muted-foreground text-sm', className)} {...props}>
+    <p className={cn("text-muted-foreground text-sm", className)} {...props}>
       {children}
     </p>
   );
