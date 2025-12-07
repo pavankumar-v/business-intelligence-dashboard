76731681e82a0ec693099d7479df6334fc85094a
Pavan Kumar V <pavankumarv124@gmail.com>
2025-12-07 22:54:35 +0530

    chore: add fallback logo

--- src/components/utils/model-logo.tsx
+++ src/components/utils/model-logo.tsx
@@ -1,26 +1,34 @@
 import claudLogo from "@/assets/claude.png";
 import chatGpt from "@/assets/models/chatgpt.png";
+import { Circle, CircleDashed } from "lucide-react";
 
 const ModelLogo = ({ name }: { name: string }) => {
+  const logo = getLogo(name.toLocaleLowerCase());
+
   return (
     <div>
-      <img
-        src={getLogo(name.toLocaleLowerCase())}
-        alt={name}
-        className="w-9 h-9 left-[-14px] top-[-5px] rounded-md bg-accent p-2 dark:bg-gray-200"
-      />
+      {logo ? (
+        <img
+          src={logo}
+          alt={name}
+          className="w-9 h-9 left-[-14px] top-[-5px] rounded-md bg-accent p-2 dark:bg-gray-200"
+        />
+      ) : (
+        <CircleDashed className="w-9 h-9 left-[-14px] top-[-5px] rounded-md bg-accent p-2 dark:bg-black" />
+      )}
     </div>
   );
 };
 
 export default ModelLogo;
 
-const getLogo = (name: string) => {
+const getLogo = (name: string): string | null => {
   if (name.includes("claude")) {
     return claudLogo;
   }
   if (name.includes("gpt")) {
     return chatGpt;
   }
-  return chatGpt;
+
+  return null;
 };
